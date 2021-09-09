8b41ba63c5dd8aa98cf11baca5c431e788afd450
ryusu777 <ryunamikaze777@gmail.com>
2021-09-09 23:48:35 +0700

    Routing

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="antialiased">
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <h1 class="text-gray-200 col">Hello from homepage</h1>
+  </div>
+</body>
+</html>
